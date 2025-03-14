f0d8cf280eecea36c07fd3763f6b8dcf237cc11e
Cameron Banowsky <cameron@shebash.io>
2025-03-14 12:02:50 -0700

    Fix ESLint dependency conflicts
    - Downgrade to ESLint 8.x to resolve dependency conflicts
    - Use traditional .eslintrc.js configuration
    - Switch back to CommonJS module format
    - Update lint script with appropriate glob patterns
    - Use TypeScript ESLint plugins compatible with ESLint 8.x

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist/**/*', 'node_modules/**/*'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }],
+    'no-console': 'off',
+    'semi': ['error', 'always']
+  },
+};
